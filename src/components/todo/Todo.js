c718cd94ef68569d7b6e4763159e0fa51d562109
anangyoga <aharwinto@gmail.com>
2021-10-18 00:30:32 +0700

    learn how to create event handler and then pass data to parent component

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,3 +1,4 @@
+import TodoCreate from "../todo-create/TodoCreate";
 import TodoList from "../todo-list/TodoList";
 import "./Todo.css";
 
@@ -16,11 +17,21 @@ const Todo = () => {
       id: 3,
       title: "Work",
     },
+    {
+      id: 4,
+      title: "Code",
+    },
   ];
 
+  const eventCreateTodo = (todo) => {
+    todos.push(todo);
+    console.log(todos);
+  };
+
   return (
-    <div className="">
+    <div>
       <h3>To-do List</h3>
+      <TodoCreate onCreateTodo={eventCreateTodo} />
       <TodoList dataTodos={todos} />
     </div>
   );
